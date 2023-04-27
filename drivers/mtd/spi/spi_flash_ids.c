f2fdddd8ba8e35770f75e7c4122f5bb0871768c5
MengShu <35624193+RookieMS@users.noreply.github.com>
2023-04-27 12:00:20 +0800

    Update spi_flash_ids.c

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -66,6 +66,7 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"gd25q64b",	   INFO(0xc84017, 0x0, 64 * 1024,   128, SECT_4K) },
 	{"gd25q32b",       INFO(0xc84016, 0x0, 64 * 1024,    64, SECT_4K) },
 	{"gd25q128b",	   INFO(0xc84018, 0x0, 64 * 1024,   256, SECT_4K) },
+	{"gd25q256e",	   INFO(0xc84019, 0x0, 64 * 1024,   512, SECT_4K) },
 	{"gd25lq32",	   INFO(0xc86016, 0x0, 64 * 1024,    64, SECT_4K) },
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
